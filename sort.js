10a66e2ca04a743acfea5d839b208ef7dfca3be4
sairammacharla <sairammacharla@gmail.com>
2016-08-08 20:33:39 +0530

    Update sort.js

--- sort.js
+++ sort.js
@@ -3,12 +3,12 @@ console.log(sortArray(arr));
 function sortArray(arr){
 	for (var i = 1; i < arr.length; i++) {
  
-      if (arr[i] < arr[i - 1]) {
-        arr[i] = arr[i] + arr[i - 1];
-        arr[i - 1] = arr[i] - arr[i - 1];
-        arr[i] = arr[i] - arr[i - 1];
-        i = 0;
-      }
+	      if (arr[i] < arr[i - 1]) {
+	        arr[i] = arr[i] + arr[i - 1];
+	        arr[i - 1] = arr[i] - arr[i - 1];
+	        arr[i] = arr[i] - arr[i - 1];
+	        i = 0;
+	      }
 	
 	}
 	return arr;
